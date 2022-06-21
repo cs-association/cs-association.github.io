b6ce69afb9a86aff3d093765b290224541f99584
Anirudh A V <76550448+Anirudh-A-V@users.noreply.github.com>
2022-06-22 01:02:39 +0530

    Slider almost complete.. Arrows left.. Footer Desgin is incomplete.. Basic Functions are implemented

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,136 @@
 import React from "react";
 import "./footer.css";
+// import { FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter, FaYoutube } from "react-icons/fa";
+import Facebook from '../../icons/Facebook.svg';
+import Facebook_active from '../../icons/Facebook_active.svg';
+import Youtube from '../../icons/Youtube.svg';
+import Youtube_active from '../../icons/Youtube_active.svg';
+import Instagram from '../../icons/Instagram.svg';
+import Instagram_active from '../../icons/Instagram_active.svg';
+import LinkedIn from '../../icons/LinkedIn.svg';
+import LinkedIn_active from '../../icons/LinkedIn_active.svg';
+import Twitter from '../../icons/Twitter.svg';
+import Twitter_active from '../../icons/Twitter_active.svg';
 
+class Hover extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isHover: false
+    };
+
+    this.onMouseEnterHandler = this.onMouseEnterHandler.bind(this);
+    this.onMouseLeaveHandler = this.onMouseLeaveHandler.bind(this);
+  }
+
+  onMouseEnterHandler() {
+    this.setState({
+      isHover: true
+    });
+  }
+
+  onMouseLeaveHandler() {
+    this.setState({
+      isHover: false
+    });
+  }
+
+}
+
+const footer = new Hover();
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="footer" style={{ background: "#6906B7" }}>
+        <div className="footer-content">
+          <div className="footer-content-main">
+            <h5>Contents</h5>
+            <li>
+              <h6>Contact Us</h6>
+              <h7>+91 1234567890</h7><br />
+              <h7>+91 1234567890</h7>
+            </li>
+            <li>
+              <h6>Sponsors</h6>
+              <h7>GTech &mu;Learn</h7>
+            </li>
+          </div>
+          <h6 className="tag">Follow for more updates</h6>
+          <div className="social-media">
+            <li>
+              {/* <a href="https://www.facebook.com/">
+              <FaFacebook className="social-icons"/>
+            </a> */}
+
+              <div className="icon" onMouseEnter={footer.onMouseEnterHandler} onMouseLeave={footer.onMouseLeaveHandler}>
+                <a href="https://www.facebook.com/">
+                  {footer.state.isHover
+                    ? <img src={Facebook_active} alt="facebook" className="social-icons" />
+                    : <img src={Facebook} alt="facebook" className="social-icons" />
+                  }
+                </a>
+              </div>
+            </li>
+            <li>
+              {/* <a href="https://www.instagram.com/">
+              <FaInstagram className="social-icons" />
+            </a> */}
+              <div className="icon" onMouseEnter={footer.onMouseEnterHandler} onMouseLeave={footer.onMouseLeaveHandler}>
+                <a href="https://www.instagram.com/">
+                  {footer.state.isHover
+                    ? <img src={Instagram_active} alt="Instagram" className="social-icons" />
+                    : <img src={Instagram} alt="Instagram" className="social-icons" />
+                  }
+                </a>
+              </div>
+            </li>
+            <li>
+              {/* <a href="https://www.linkedin.com/">
+                <FaLinkedinIn className="social-icons" />
+              </a> */}
+              <div className="icon" onMouseEnter={footer.onMouseEnterHandler} onMouseLeave={footer.onMouseLeaveHandler}>
+                <a href="https://www.linkedin.com/">
+                  {footer.state.isHover
+                    ? <img src={LinkedIn_active} alt="LinkedIn" className="social-icons" />
+                    : <img src={LinkedIn} alt="LinkedIn" className="social-icons" />
+                  }
+                </a>
+              </div>
+            </li>
+            <li>
+              {/* <a href="https://www.twitter.com/">
+                <FaTwitter className="social-icons" />
+              </a> */}
+              <div className="icon" onMouseEnter={footer.onMouseEnterHandler} onMouseLeave={footer.onMouseLeaveHandler}>
+                <a href="https://www.twitter.com/">
+                  {footer.state.isHover
+                    ? <img src={Twitter_active} alt="Twitter" className="social-icons" />
+                    : <img src={Twitter} alt="Twitter" className="social-icons" />
+                  }
+                </a>
+              </div>
+            </li>
+            <li>
+              {/* <a href="https://www.youtube.com/">
+                <FaYoutube className="social-icons" />
+              </a> */}
+              <div className="icon" onMouseEnter={footer.onMouseEnterHandler} onMouseLeave={footer.onMouseLeaveHandler}>
+                <a href="https://www.youtube.com/">
+                  {footer.state.isHover
+                    ? <img src={Youtube_active} alt="Youtube" className="social-icons" />
+                    : <img src={Youtube} alt="Youtube" className="social-icons" />
+                  }
+                </a>
+              </div>
+            </li>
+          </div>
+        </div>
+      </div>
+
+
+    </>
+  );
 };
 
 export default Footer;
--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,8 +1,50 @@
 import React from "react";
+import 'react-multi-carousel/lib/styles.css';
 import "./slider.css";
 
+const Card = (props) => (
+  <div className="card">
+    <img src={ props.imgUrl } 
+      alt={ props.alt || 'Image' } />
+    <div className="card-content">
+    </div>
+  </div>
+);
+
+const CardContainer = (props) => (
+  <div className="cards-container">
+    {
+      props.cards.map((card) => (
+        <Card title={ card.title }
+          imgUrl={ card.imgUrl } />
+      ))
+    }
+  </div>
+);
+
+const cardsData = [
+  {id: 1, title: 'CARD 1',imgUrl: 'https://rb.gy/lhg0ay'},
+  {id: 2, title: 'CARD 2',imgUrl: 'https://rb.gy/ucks3o'},
+  {id: 3, title: 'CARD 3', imgUrl: 'https://rb.gy/sj6lxm'},
+  {id: 4, title: 'CARD 4', imgUrl: 'https://rb.gy/apacdc'},
+  {id: 5, title: 'CARD 5', imgUrl: 'https://rb.gy/y2vgum'},
+  {id: 6, title: 'CARD 6', imgUrl: 'https://rb.gy/xrceqy'},
+  {id: 7, title: 'CARD 7', imgUrl: 'https://rb.gy/jontot'},
+  {id: 8, title: 'CARD 8', imgUrl: 'https://rb.gy/exo8kl'},
+  {id: 9, title: 'CARD 9', imgUrl: 'https://rb.gy/vvfclq'},
+  {id: 10, title: 'CARD 10', imgUrl: 'https://rb.gy/jrkh3d'},
+]
+
 const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <div className="container">
+        {/* <h1 style={{ 'text-align': 'center' }}>
+          Posters
+        </h1> */}
+        
+        <CardContainer cards={ cardsData } />
+      </div>
+      );
 };
 
 export default Slider;
