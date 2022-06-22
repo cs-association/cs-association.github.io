ae3ec3101dfaca72d56fd2bc74a060474810501b
jaidan22 <jaidansleeba22@gmail.com>
2022-06-22 23:08:23 +0530

    integrated api

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./body.css";
 import drishti from "../../images/drishti-logo.svg";
 import cs from "../../images/cs.svg";
 import Card from "../card/Card";
+import axios from "axios";
 
 const Body = () => {
+  const [data, setData] = useState({ score: [] });
+
+  useEffect(() => {
+    const getdata = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.REACT_APP_API_URI}/scoreboard`
+        );
+        setData({ ...res.data });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getdata();
+  }, []);
+
   return (
     <div className="body">
       <div className="header">
@@ -13,19 +30,11 @@ const Body = () => {
       </div>
 
       <div className="cards">
-      <span className="games-title">GAMES</span>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        
+        <span className="games-title">GAMES</span>
+        {data.score.map((item) => {
+          console.log(item);
+          return <Card item={item} />;
+        })}
       </div>
     </div>
   );
