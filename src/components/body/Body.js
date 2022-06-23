8643f647c7ff6ab675b2f0b67dfa933b973a0954
jaidan22 <jaidansleeba22@gmail.com>
2022-06-23 09:49:09 +0530

    added sponsors logo

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -4,24 +4,35 @@ import drishti from "../../images/drishti-logo.svg";
 import cs from "../../images/cs.svg";
 import Card from "../card/Card";
 import axios from "axios";
+import litmus from "../../images/litmus.svg";
+import entri from "../../images/entri.svg";
+import edgradi from "../../images/edgradi.svg";
 
 const Body = () => {
   const [data, setData] = useState({ score: [] });
   const loadRef = useRef();
 
+  const startloading = () => {
+    loadRef.current.style.visibility = "visible";
+    loadRef.current.style.opacity = 1;
+  };
+
+  const endloading = () => {
+    loadRef.current.style.opacity = 0;
+    loadRef.current.style.visibility = "hidden";
+  };
+
   useEffect(() => {
     const getdata = async () => {
       try {
-        loadRef.current.style.visibility = "visible";
-        loadRef.current.style.opacity = 1;
-
+        startloading();
         const res = await axios.get(
           `${process.env.REACT_APP_API_URI}/scoreboard`
         );
         setData({ ...res.data });
-
-        loadRef.current.style.opacity = 0;
-        loadRef.current.style.visibility = "hidden";
+        setTimeout(() => {
+          endloading();
+        }, 1000);
       } catch (err) {
         console.log(err);
       }
@@ -50,6 +61,12 @@ const Body = () => {
 
       <div className="header">
         <img src={drishti} className="img img-drishti" alt="" />
+        <span>DRISHTI 2022</span>
+        <div className="sponsors">
+          <img className="img-sponsor" src={litmus} alt="" />
+          <img className="img-sponsor" src={entri} alt="" />
+          <img className="img-sponsor" src={edgradi} alt="" />
+        </div>
         <img src={cs} className="img img-cs" alt="" />
       </div>
 
