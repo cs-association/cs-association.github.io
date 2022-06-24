6124e155116fd2ac2db63385832520d60cfd8287
cs-association <anirudhpchalla3@gmail.com>
2022-06-24 17:10:25 +0530

    footer code optimized

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 import "./footer.css";
-// import { FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter, FaYoutube } from "react-icons/fa";
 import Facebook from '../../icons/Facebook.svg';
 import Facebook_active from '../../icons/Facebook_active.svg';
 import Youtube from '../../icons/Youtube.svg';
@@ -10,36 +8,9 @@ import Instagram from '../../icons/Instagram.svg';
 import Instagram_active from '../../icons/Instagram_active.svg';
 import LinkedIn from '../../icons/LinkedIn.svg';
 import LinkedIn_active from '../../icons/LinkedIn_active.svg';
-import Map from '../../images/Map.png';
+// import Map from '../../images/Map.png';
 import Ground_Floor_Final from '../../images/Ground_Floor_Final.png';
 
-class Hover extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      isHover: false
-    };
-
-    this.onMouseEnterHandler = this.onMouseEnterHandler.bind(this);
-    this.onMouseLeaveHandler = this.onMouseLeaveHandler.bind(this);
-  }
-
-  onMouseEnterHandler() {
-    this.setState({
-      isHover: true
-    });
-  }
-
-  onMouseLeaveHandler() {
-    this.setState({
-      isHover: false
-    });
-  }
-
-}
-
-const footer = new Hover();
 const Footer = () => {
   return (
     <>
@@ -67,32 +38,24 @@ const Footer = () => {
 
             <div className="social-media">
               <li>
-                <div className="icon" onMouseEnter={footer.onMouseEnterHandler} onMouseLeave={footer.onMouseLeaveHandler}>
                   <a href="https://www.facebook.com/drishticet/" target='_blank' rel="noreferrer">
                   <img src={Facebook} onMouseLeave={e=>(e.currentTarget.src=Facebook)} onMouseOver={e=>(e.currentTarget.src=Facebook_active)} alt="Facebook" className="social-icons" />
                   </a>
-                </div>
               </li>
               <li>
-                <div className="icon" onMouseEnter={footer.onMouseEnterHandler} onMouseLeave={footer.onMouseLeaveHandler}>
                   <a href="https://www.instagram.com/drishticet/" target='_blank' rel="noreferrer">
                   <img src={Instagram} onMouseLeave={e=>(e.currentTarget.src=Instagram)} onMouseOver={e=>(e.currentTarget.src=Instagram_active)} alt="Instagram" className="social-icons" />
                   </a>
-                </div>
               </li>
               <li>
-                <div className="icon" onMouseEnter={footer.onMouseEnterHandler} onMouseLeave={footer.onMouseLeaveHandler}>
                   <a href="https://www.linkedin.com/company/drishticet/" target='_blank' rel="noreferrer">
                   <img src={LinkedIn} onMouseLeave={e=>(e.currentTarget.src=LinkedIn)} onMouseOver={e=>(e.currentTarget.src=LinkedIn_active)} alt="LinkedIn" className="social-icons" />
                   </a>
-                </div>
               </li>
               <li>
-                <div className="icon" onMouseEnter={footer.onMouseEnterHandler} onMouseLeave={footer.onMouseLeaveHandler}>
                   <a href="https://www.youtube.com/drishticet" target='_blank' rel="noreferrer">
                   <img src={Youtube} onMouseLeave={e=>(e.currentTarget.src=Youtube)} onMouseOver={e=>(e.currentTarget.src=Youtube_active)} alt="Youtube" className="social-icons" />
                   </a>
-                </div>
               </li>
             </div>
           </div>
